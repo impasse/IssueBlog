27a0bcf466226062f8f8a3e0c81cbd0e8bff750b
ling <i@shyling.com>
2016-12-14 14:42:46 +0800

    Update const.js

--- src/const.js
+++ src/const.js
@@ -30,6 +30,7 @@ let links = `
 <li><a href="https://www.moew.xyz" target="_blank">夜雨初霁的小窝</a></li>
 <li><a href="http://ouzz.me" target="_blank">ouzz的博客</a></li>
 <li><a href="http://anneke.cn" target="_blank">卷猫's blog</a></li>
+<li><a href="http://blog.zhouys.ac.cn" target="_blank">東雲研究所</a></li>
 </ul>
 `;
 
